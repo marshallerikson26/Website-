dc7c3750dc2d1f1573a3e41d66c8055bed0af278
marshallerikson26 <marshallerikson26@gmail.com>
2025-07-24 19:40:22 +0200

    Delete _site/Script.js

--- _site/Script.js
+++
@@ -1,9 +0,0 @@
-function readMore(id) {
-  alert('Opening full article #' + id + ' (this can be upgraded later)');
-}
-
-function submitForm(event) {
-  event.preventDefault();
-  alert('Thank you for contacting us!');
-  return false;
-}
